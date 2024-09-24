57edb767ae3e078236e9f07863f4bf09ea7f822d
AnataAria <kakata28419@gmail.com>
2024-09-24 16:20:33 +0700

    Init Project

--- app/src/main/java/com/theanimegroup/horse_racing_client/controller/CashActivity.java
+++ app/src/main/java/com/theanimegroup/horse_racing_client/controller/CashActivity.java
@@ -10,17 +10,11 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.theanimegroup.horse_racing_client.R;
 
-public class MainActivity extends AppCompatActivity {
-
+public class CashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        setContentView(R.layout.cash_layout);
     }
 }
---
+++ app/src/main/java/com/theanimegroup/horse_racing_client/controller/GamePlayActivity.java
@@ -0,0 +1,20 @@
+package com.theanimegroup.horse_racing_client.controller;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.theanimegroup.horse_racing_client.R;
+
+public class GamePlayActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.gameplay_layout);
+    }
+}
