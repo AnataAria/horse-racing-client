4753044d7409829beb3488de14f72edc94035eb1
zacgnOoO <zachkey462003@gmail.com>
2024-09-28 20:54:42 +0700

    Add result game

--- app/src/main/java/com/theanimegroup/horse_racing_client/controller/ResultActivity.java
+++ app/src/main/java/com/theanimegroup/horse_racing_client/controller/ResultActivity.java
@@ -4,17 +4,14 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.theanimegroup.horse_racing_client.R;
 
-public class GamePlayActivity extends AppCompatActivity {
+public class ResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.gameplay_layout);
+        setContentView(R.layout.result_layout);
     }
 }
